5977e1f4a4e322bb7b58034511e53f65e8564027
Eric Warmenhoven <eric@warmenhoven.org>
2023-12-12 10:31:38 -0500

    MAYBE PR - add .dir-locals.el for emacs formatting

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+(
+ (c-mode . ((c-basic-offset . 3)))
+ (objc-mode . ((c-basic-offset . 3)))
+ )
